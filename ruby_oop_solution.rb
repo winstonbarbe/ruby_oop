a2fcabc4b113eb8bfcd02386a4651999d243bf5c
winstonbarbe <winston.barbe@gmail.com>
2024-12-22 15:49:00 -0500

    adds person class and solves for requirements in readme file

--- ruby_oop_solution.rb
+++ ruby_oop_solution.rb
@@ -7,8 +7,13 @@ class Animal
   end
 
   def eat(food)
+    if @type == "person" && food == "person"
+      puts "I can not eat that food because i am a person"
+      return
+    end
     puts "Mmm i'm eating #{food}"
   end
+
   def sleep
     @state = "asleep"
   end
@@ -16,7 +21,18 @@ class Animal
     @state = "awake"
   end
 end
-
+class Person < Animal
+  def initialize(input_options)
+    super
+    @type = "person"
+    @age = input_options[:age]
+    @gender = input_options[:gender]
+    @name = input_options[:name]
+  end
+  def greet
+    puts "Hi, my name is #{@name} and I am a #{@gender} who is #{@age} years old."
+  end
+end
 
 animal1 = Animal.new(type: "dog")
 p animal1.type
@@ -25,3 +41,10 @@ animal1.eat("kibble")
 p animal1.state
 animal1.sleep
 p animal1.state
+
+person1 = Person.new(type: "cow", age: 190, gender: "male", name: "Bessie")
+p person1.type
+
+person1.eat("grass")
+person1.eat("person")
+person1.greet
